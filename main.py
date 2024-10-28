2a704bcc7e5e9adc901c92d56665bf66c835f1be
Nenela27 <daniwharu2023@gmail.com>
2024-10-28 22:30:55 +0800

    B: 2.5 Draw Player

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 # this allows us to use code from the open-source pygame library
 # throughout this file
 from constants import *
+from player import Player
 import pygame
 
 def main():
     pygame.init()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()

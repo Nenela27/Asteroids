29dd82c26ff7c716d840016c3a3aceb5d26ffc8c
Nenela27 <daniwharu2023@gmail.com>
2024-12-15 19:03:23 +0800

    A: Game Loop

---
+++ main.py
@@ -0,0 +1,25 @@
+# this allows us to use code from the open-source pygame library throughout this file
+import pygame
+from constants import *
+
+def main():
+    # Initialize Pygame
+    pygame.init()
+
+    # Set up the display
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return
+
+    # Game loop
+    while True:
+        # Fill the screen with black
+        screen.fill((0, 0, 0))
+
+        # Refresh the display
+        pygame.display.flip() 
+
+if __name__ == "__main__":
+    main()

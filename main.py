187f2b612e3eb485c4aeec700a3663e8a7db26a2
Nenela27 <daniwharu2023@gmail.com>
2024-12-15 20:00:29 +0800

    C: Player able to move around

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # this allows us to use code from the open-source pygame library throughout this file
 import pygame
+
 from constants import *
+from player import Player
 
 def main():
     # Initialize Pygame
@@ -8,18 +10,26 @@ def main():
 
     # Set up the display
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
 
     # Game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+        
+        player.update(dt)
         # Fill the screen with black
         screen.fill((0, 0, 0))
+        player.draw(screen)
 
         # Refresh the display
-        pygame.display.flip() 
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
